50f664b9ef66f9a10d26eeb4f53eb470573eb9ee
NeoAbhiS <abhijit.satamkar@gmail.com>
2021-08-09 18:28:20 +0200

    Update Course Controller

--- src/main/java/com/springboot/springweb/courses/controller/CourseController.java
+++ src/main/java/com/springboot/springweb/courses/controller/CourseController.java
@@ -12,13 +12,20 @@ import com.springboot.springweb.courses.bean.Course;
 public class CourseController {
 	
 	// http://localhost:8080/courses
-	
 	@GetMapping("/courses")
 	public List<Course> getCourses()
 	{
-		return Arrays.asList(new Course(1, "SpringBoot", "java"), new Course(2, "panda", "python"));
+		return Arrays.asList(new Course(1, "SpringBoot", "java"), 
+				new Course(2, "panda", "python"),
+				new Course(3, "Angular", "javaScript"));
 	}
 	
+	// http://localhost:8080/course/1
+	@GetMapping("/course/1")
+	public Course getCourse()
+	{
+		return new Course(1, "SpringBoot", "java");
+	}
 	
 	
 
